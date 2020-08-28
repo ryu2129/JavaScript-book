4ac612214667f1733d9981ca486ddcd748320594
Ryunosuke Abe <ryu212920@gmail.com>
2020-08-28 20:33:24 +0900

    finish 3-6

--- tutorial/bouncing-balls/main.js
+++ tutorial/bouncing-balls/main.js
@@ -1,23 +1,23 @@
 // setup canvas
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
 function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  var num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
 
 function Ball(x, y, velX, velY, color, size) {
   this.x = x;
   this.y = y;
-  this.velX = velX;
-  this.velY = velY;
+  this.velX = velX; //水平速度
+  this.velY = velY; //垂直速度
   this.color = color;
   this.size = size;
 }
@@ -25,6 +25,70 @@ function Ball(x, y, velX, velY, color, size) {
 Ball.prototype.draw = function() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2, * Math.PI);
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
+};
+
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+Ball.prototype.collisionDetect = function() {
+  for (var j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
 }
+
+var balls = [];
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  ctx.fillRect(0, 0, width, height);
+
+  while (balls.length < 13) {
+    var size = random(10, 20);
+    var ball = new Ball(
+      random(0 + size,width - size),
+      random(0 + size,height - size),
+      random(-7,7),
+      random(-7,7),
+      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')',
+      size
+    );
+    balls.push(ball);
+  }
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+loop();
