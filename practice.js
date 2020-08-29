1dbe5622c5532c3bf82c4acf522b0bd1b8f8fa7f
Ryunosuke Abe <ryu212920@gmail.com>
2020-08-29 14:08:04 +0900

    book 4-4

--- practice.js
+++ practice.js
@@ -1,2 +1,21 @@
 var data = ['aa', 'bb']
 console.log(data[0])
+
+var ary = [5,25,10];
+console.log(ary.sort());
+console.log(ary.sort(function(x,y) {
+  return x - y;
+}));
+
+function showMessage(value) {
+  if (arguments.length !== 1) {
+    throw new Error('引数の数が間違っています。:' + arguments.length);
+  }
+  console.log(value);
+}
+
+try {
+  showMessage('山田', '鈴木');
+} catch(e) {
+  window.alert(e.message);
+}
